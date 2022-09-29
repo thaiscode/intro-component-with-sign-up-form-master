bd54363596d15d2f27aeaafe62acdee89a44e70c
Thais Silva <thais_ap@live.com>
2022-09-28 21:57:20 -0300

    style done

---
+++ script.js
@@ -0,0 +1,17 @@
+let input = document.querySelectorAll('input')
+
+let submit = document.querySelector('.form__box-button')
+
+
+
+
+for (let i = 0; i < button.length; i++) {
+    input[i].addEventListener('click', submit)
+}
+
+function submit {
+
+}
+
+
+
